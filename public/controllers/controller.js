49be8fcdfbb605e638a0579063b4f083428559ca
Sagar <sagar.bhoite@sjsu.edu>
2016-05-03 00:33:24 -0700

    First commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,266 @@
+var app = angular.module('myApp', ["highcharts-ng"]);
+
+app.controller('MyCtrl', ['$scope', '$interval', '$timeout', '$http',
+    function($scope, $interval, $timeout, $http) {
+        $scope.resultData = [];
+        $scope.actionType = [];
+        $scope.actionCount = [];
+        $scope.activityDistrubutionFlag = false;
+        $scope.aboutFlag = false;
+        $scope.homeFlag = true;
+        $scope.chartType = "View Line Chart";
+        $scope.title = "Dashboard";
+        $scope.actionsMap = {
+            "idle": 0,
+            "walking": 1,
+            "running": 2,
+            "sitting": 3,
+            "sleeping": 4,
+            "ascending stairs": 5,
+            "descending stairs": 6,
+            "standing": 7
+        }
+        $scope.currentActionLabel = "idle";
+
+        var activityDistrubutionInterval;
+
+        //Pie chart start
+        function getData() {
+            $http.get('/chartData').success(function(data) {
+                $scope.resultData = [];
+                $scope.actionType.length = 0;
+                $scope.actionCount.length = 0;
+                for (var key in data) {
+                    $scope.resultData.push([capitalizeEachWord(key), parseInt(data[key])]);
+                    $scope.actionType.push(capitalizeEachWord(key));
+                    $scope.actionCount.push(parseInt(data[key]));
+                }
+                //data for pie chart
+                $scope.pieActions = $scope.resultData;
+
+                //data for bar chart
+
+            }).error(function(data) {
+                console.log('Error: ' + data);
+            });
+        }
+
+        function capitalizeEachWord(str) {
+            return str.replace(/\w\S*/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        }
+        //Pie chart end
+
+        //Click Events
+        $scope.activityDistrubution = function() {
+            $scope.title = "Activity Distribution";
+            getData();
+            activityDistrubutionInterval = $interval(getData, 10000);
+            $scope.activityDistrubutionFlag = true;
+            $scope.aboutFlag = false;
+            $scope.homeFlag = false;
+            currentActionInterval = undefined;
+        };
+
+        $scope.home = function() {
+            $scope.title = "Dashboard";
+            $scope.activityDistrubutionFlag = false;
+            $scope.aboutFlag = false;
+            $scope.homeFlag = true;
+            getCurrentAction();
+            currentActionInterval = $interval(getCurrentAction, 3000);
+            activityDistrubutionInterval = undefined;
+        };
+        $scope.home();
+
+        $scope.userProfile = function() {
+            $scope.title = "Profile";
+            $scope.activityDistrubutionFlag = false;
+            $scope.aboutFlag = false;
+            $scope.homeFlag = false;
+            activityDistrubutionInterval = undefined;
+            currentActionInterval = undefined;
+        };
+
+        $scope.activityPerDay = function() {
+            $scope.title = "Activity Per Day";
+            $scope.activityDistrubutionFlag = false;
+            $scope.aboutFlag = false;
+            $scope.homeFlag = false;
+            activityDistrubutionInterval = undefined;
+            currentActionInterval = undefined;
+        };
+
+        $scope.health = function() {
+            $scope.title = "Health";
+            $scope.activityDistrubutionFlag = false;
+            $scope.aboutFlag = false;
+            $scope.homeFlag = false;
+            activityDistrubutionInterval = undefined;
+            currentActionInterval = undefined;
+        };
+
+        $scope.about = function() {
+            $scope.title = "About";
+            $scope.activityDistrubutionFlag = false;
+            $scope.aboutFlag = true;
+            $scope.homeFlag = false;
+            activityDistrubutionInterval = undefined;
+            currentActionInterval = undefined;
+        };
+        //Click Events
+
+        function getCurrentAction() {
+            $http.get('/currentAction').success(function(data) {
+                $scope.actionSrc = "assets/gifs/" + data + ".gif";
+                $scope.currentActionLabel = data;
+            }).error(function(data) {
+                console.log('Error: ' + data);
+            });
+        }
+
+        //Bar chart start
+        $scope.options = {
+            type: 'column'
+        }
+
+        $scope.swapChartType = function() {
+            if (this.highchartsNG.options.chart.type === 'line') {
+                this.highchartsNG.options.chart.type = 'column';
+                $scope.chartType = "View Line Chart";
+            } else {
+                this.highchartsNG.options.chart.type = 'line';
+                $scope.chartType = "View Bar Chart";
+            }
+        }
+
+        $scope.highchartsNG = {
+            options: {
+                chart: {
+                    type: 'column',
+                    options3d: {
+                        enabled: true,
+                        alpha: 15,
+                        beta: 0,
+                        depth: 100,
+                        viewDisatance: 25
+                    }
+                }
+            },
+            series: [{
+                data: $scope.actionCount,
+                name: "Activities"
+            }],
+            xAxis: {
+                categories: $scope.actionType
+            },
+            title: {
+                text: 'Activity Distribution'
+            },
+            plotOptions: {
+                column: {
+                    depth: 25
+                }
+            },
+            loading: false
+        }
+        //Bar chart end
+
+        //Spline chart start
+        $scope.addPoints = function () {
+            var seriesArray = $scope.chartConfig.series;
+            var rndIdx = Math.floor(Math.random() * seriesArray.length);
+            seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([$scope.actionsMap[$scope.currentActionLabel]]);
+            seriesArray[rndIdx].name = capitalizeEachWord($scope.currentActionLabel);
+            $timeout($scope.addPoints, 1000);
+        };
+
+        $scope.chartSeries = [
+            {"name": capitalizeEachWord($scope.currentActionLabel), "data": [0]},
+        ];
+
+        $scope.chartConfig = {
+            options: {
+              chart: {
+                type: 'spline'
+              },
+              plotOptions: {
+                series: {
+                  stacking: ''
+                }
+              }
+            },
+            series: $scope.chartSeries,
+            title: {
+              text: 'Current Activity'
+            },
+            credits: {
+              enabled: true
+            },
+            loading: false,
+            size: {}
+        }
+
+        $scope.addPoints();
+        //Spline chart end
+    }
+]);
+
+app.directive('hcPie', function() {
+    return {
+        restrict: 'C',
+        replace: true,
+        scope: {
+            items: '='
+        },
+        controller: function($scope, $element, $attrs) {},
+        template: '<div id="container" style="margin: 0 auto">not working</div>',
+        link: function(scope, element, attrs) {
+
+            var chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container',
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie',
+                    options3d: {
+                        enabled: true,
+                        alpha: 45
+                    }
+                },
+                title: {
+                    text: 'Activity Tracker'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>',
+                    percentageDecimals: 2
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            color: '#000000',
+                            connectorColor: '#000000',
+                            formatter: function() {
+                                return '<b>' + this.point.name + '</b>: ' + parseFloat(this.percentage.toFixed(2)) + ' %';
+                            }
+                        },
+                        innersize: 100,
+                        depth: 45
+                    }
+                },
+                series: [{
+                    name: 'Activity Distrubution',
+                    data: scope.items
+                }]
+            });
+            scope.$watch("items", function(newValue) {
+                chart.series[0].setData(newValue, true);
+            }, true);
+        }
+    }
+});
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    request = require('request');
+var app = express();
+
+var cassandra = require('cassandra-driver');
+var client = new cassandra.Client({
+    contactPoints: ['104.197.95.54'],
+    keyspace: 'activityrecognition'
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/chartData', function(req, res) {
+    var actionsWithCount = "";
+    // Read data and print to console
+    client.execute("select group_and_count(prediction) from result", function(err, result) {
+        if (!err) {
+            if (result.rows.length > 0) {
+                var data = result.rows[0];
+                for (x in data) {
+                    actionsWithCount = data[x];
+                    break;
+                }
+                res.send(actionsWithCount);
+            } else {
+                console.log("No results");
+            }
+        }
+    });
+});
+
+app.get('/currentAction', function(req, res) {
+    // Read data and print to console
+    client.execute("select * from result where userid='TEST_USER' order by timestamp desc LIMIT 1;", function(err, result) {
+        if (!err) {
+            if (result.rows.length > 0) {
+                var data = result.rows[0];
+                res.send(data.prediction);
+            } else {
+                console.log("No results");
+            }
+        }
+    });
+});
+
+var server = app.listen(process.env.PORT || 5000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
